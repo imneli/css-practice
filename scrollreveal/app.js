37dcad0c43f4b24e59fbc86062b612fc4bc14f22
imneli <contatodoneli@gmail.com>
2024-05-01 15:20:57 -0300

    scrollreveal

---
+++ scrollreveal/app.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({reset: true });
+
+
+sr.reveal('.area-1', {
+    rotate: {x: 0, y: 80, z: 0,
+    duration: 3000}
+});
+sr.reveal('.area-2', { duration: 1000 });
+sr.reveal('.area-3', {
+    rotate: {x: 0, y: 100, z: 0,
+    duration: 3000 }
+    });
+
